38acdd3dd80b282339358f9c6b207295e16589a3
Cat <jarolika30@gmail.com>
2018-10-31 16:13:57 +0300

    next part of lesson

--- object/script.js
+++ object/script.js
@@ -14,4 +14,16 @@ console.log(typeof person);
 console.log(person);
 person.canCode = true;
 console.log(person);
-document.body.innerHTML = person.name + " " + person.surname + " " + person.age;
+const par = document.querySelector('p');
+par.innerHTML = person.name + " " + person.surname + " " + person.age;
+const cools = document.getElementsByClassName('cool');
+console.log(cools);
+console.log(cools[1]);
+cools[0].innerHTML = "I am first";
+const cools1 = document.getElementsByTagName('div');
+console.log(cools1);
+cools1[1].innerHTML = "I am glad";
+
+const cools3 = document.querySelectorAll('.cool');
+console.log(cools3);
+cools3[2].innerHTML = "I am fine";
